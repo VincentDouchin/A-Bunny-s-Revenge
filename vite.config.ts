8cd95f534ae69d2bb439939fc579b7e13f974188
Vincent Douchin <vincent.douchin@gmail.com>
2023-12-31 13:53:16 +0100

    add vite pwa

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,63 @@ import type { UserConfig } from 'vite'
 import { defineConfig } from 'vite'
 import solidPlugin from 'vite-plugin-solid'
 
-// import { VitePWA } from 'vite-plugin-pwa'
+import { VitePWA } from 'vite-plugin-pwa'
 import generateAssetNames from './scripts/generateAssetNamesPlugin'
 import { autoConvertFBXtoGLB } from './scripts/convertFbx2GLB'
 
-// https://vitejs.dev/config/
 export default defineConfig(() => {
 	const config: UserConfig = {
-		plugins: [generateAssetNames(), autoConvertFBXtoGLB(), solidPlugin()],
+		plugins: [
+			generateAssetNames(),
+			autoConvertFBXtoGLB(),
+			solidPlugin(),
+			VitePWA({
+				registerType: 'autoUpdate',
+				injectRegister: 'auto',
+				includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+				injectManifest: {
+					globPatterns: ['**/*.{js,html,wasm}', './assets/*.*'],
+				},
+				manifest: {
+					start_url: 'index.html?fullscreen=true',
+					display: 'fullscreen',
+					orientation: 'landscape',
+					name: 'Time Loop RPG',
+					short_name: 'Time Loop RPG',
+					description: 'Time Loop RPG',
+					theme_color: '#000000',
+					icons: [
+						{
+							src: 'pwa-64x64.png',
+							sizes: '64x64',
+							type: 'image/png',
+						},
+						{
+							src: 'pwa-192x192.png',
+							sizes: '192x192',
+							type: 'image/png',
+						},
+						{
+							src: 'pwa-512x512.png',
+							sizes: '512x512',
+							type: 'image/png',
+							purpose: 'any',
+						},
+						{
+							src: 'maskable-icon-512x512.png',
+							sizes: '512x512',
+							type: 'image/png',
+							purpose: 'maskable',
+						},
+					],
+				},
+			}),
+		],
 		assetsInclude: ['**/*.glb'],
 		base: '',
 
 		build: {
-			target: 'esnext',
+			target: 'es6',
 
 		},
 
