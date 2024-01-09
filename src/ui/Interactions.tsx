9ba972400674796a33dbd5acfdb3c868c66aa81c
Vincent Douchin <vincent.douchin@gmail.com>
2024-01-09 17:34:27 +0100

    real fix overlay

--- src/ui/Interactions.tsx
+++ src/ui/Interactions.tsx
@@ -1,7 +1,7 @@
 import { Portal, Show } from 'solid-js/web'
 import { ForQuery } from './components/ForQuery'
 import { InputIcon } from './InputIcon'
-import { ecs } from '@/global/init'
+import { ecs, ui } from '@/global/init'
 import { playerInputMap } from '@/global/inputMaps'
 
 const interactionQuery = ecs.with('interactable', 'interactionContainer', 'position')
@@ -13,13 +13,14 @@ export const InteractionUi = () => {
 				if (entity.size) {
 					entity.interactionContainer.position.y = entity.size.y - entity.position.y
 				}
+				const dialog = ui.sync(() => entity.currentDialog)
 				return (
-					<Show when={entity.currentDialog === undefined}>
+					<Show when={!dialog()}>
 						<Portal mount={entity.interactionContainer.element}>
 							<div style={{ 'background': 'hsl(0,0%,0%,0.3)', 'padding': '0.25rem 0.5rem', 'font-size': '1.5rem', 'color': 'white', 'border-radius': '1rem', 'display': 'flex', 'gap': '0.5rem' }}>
 								<InputIcon input={input}></InputIcon>
 								<div>
-									{entity.currentDialog}
+									{entity.interactable}
 								</div>
 							</div>
 						</Portal>
