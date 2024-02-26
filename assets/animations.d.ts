bbe969b789606584179ecab0ce905de6e0492bef
Vincent Douchin <vincent.douchin@gmail.com>
2024-02-26 11:09:34 +0100

    add water

--- assets/animations.d.ts
+++ assets/animations.d.ts
@@ -4,49 +4,49 @@ interface Animations {
 
 	Armabee_Evolved: `Death` | `Fast_Flying` | `Flying_Idle` | `Headbutt` | `HitReact` | `No` | `Punch` | `Yes`
 
-	Forest_Butterfly_A: `Move` | `Idle` | `Attack` | `Damage` | `Die`
+	Forest_Butterfly_C: `Move` | `Idle` | `Attack` | `Damage` | `Die`
 
-	Forest_Butterfly_D: `Move` | `Idle` | `Attack` | `Damage` | `Die`
+	Forest_Butterfly_A: `Move` | `Idle` | `Attack` | `Damage` | `Die`
 
 	Forest_Butterfly_B: `Move` | `Idle` | `Attack` | `Damage` | `Die`
 
-	Forest_Butterfly_C: `Move` | `Idle` | `Attack` | `Damage` | `Die`
-
-	Porin_A: `Move` | `Idle` | `Attack` | `Damage` | `Die`
+	Forest_Butterfly_D: `Move` | `Idle` | `Attack` | `Damage` | `Die`
 
 	Platopo_A: `Walk` | `Idle` | `Attack` | `Damage` | `Die`
+
+	Porin_A: `Move` | `Idle` | `Attack` | `Damage` | `Die`
 
 	Platopo_B: `Walk` | `Idle` | `Attack` | `Damage` | `Die`
 
 	Platopo_C: `Walk` | `Idle` | `Attack` | `Damage` | `Die`
 
-	Porin_C: `Move` | `Idle` | `Attack` | `Damage` | `Die`
+	Porin_B: `Move` | `Idle` | `Attack` | `Damage` | `Die`
 
 	Porin_D: `Move` | `Idle` | `Attack` | `Damage` | `Die`
 
-	Porin_B: `Move` | `Idle` | `Attack` | `Damage` | `Die`
+	Porin_C: `Move` | `Idle` | `Attack` | `Damage` | `Die`
 
 	Porin_E: `Move` | `Idle` | `Attack` | `Damage` | `Die`
 
-	Racco_A: `Move` | `Idle` | `Attack` | `Damage` | `Die`
-
 	Racco_B: `Move` | `Idle` | `Attack` | `Damage` | `Die`
 
-	Racco_C: `Move` | `Idle` | `Attack` | `Damage` | `Die`
-
-	Snailo_A: `Move` | `Idle` | `Attack` | `Damage` | `Die`
+	Racco_A: `Move` | `Idle` | `Attack` | `Damage` | `Die`
 
 	Shaga_A: `Move` | `Idle` | `Attack` | `Damage` | `Die`
 
-	Shaga_B: `Move` | `Idle` | `Attack` | `Damage` | `Die`
+	Shaga_C: `Move` | `Idle` | `Attack` | `Damage` | `Die`
+
+	Snailo_A: `Move` | `Idle` | `Attack` | `Damage` | `Die`
 
 	Snailo_B: `Move` | `Idle` | `Attack` | `Damage` | `Die`
 
-	Shaga_C: `Move` | `Idle` | `Attack` | `Damage` | `Die`
+	Racco_C: `Move` | `Idle` | `Attack` | `Damage` | `Die`
+
+	Snailo_D: `Move` | `Idle` | `Attack` | `Damage` | `Die`
 
 	Snailo_C: `Move` | `Idle` | `Attack` | `Damage` | `Die`
 
-	Snailo_D: `Move` | `Idle` | `Attack` | `Damage` | `Die`
+	Shaga_B: `Move` | `Idle` | `Attack` | `Damage` | `Die`
 
 	Chest: `chest_open`
 
@@ -54,9 +54,9 @@ interface Animations {
 
 	Big_Boar_B: `Move` | `Idle` | `Attack` | `Damage` | `Die`
 
-	Big_Boar_C: `Move` | `Idle` | `Attack` | `Damage` | `Die`
-
 	BunnydAnim: `idle` | `picking_vegetables` | `run` | `T_Pose`
+
+	Big_Boar_C: `Move` | `Idle` | `Attack` | `Damage` | `Die`
 
 	SampleScene: `Move` | `Idle` | `Attack` | `Damage` | `Die`
 
