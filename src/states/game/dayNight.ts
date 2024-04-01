580290fe4f5ff561c9f3fcfd1aad80e766bb7123
Vincent Douchin <vincent.douchin@gmail.com>
2024-04-01 14:13:50 +0200

    adjust overlay opacity

--- src/states/game/dayNight.ts
+++ src/states/game/dayNight.ts
@@ -5,11 +5,14 @@ const nightLightQuery = ecs.with('nightLight')
 const emissiveMatQuery = ecs.with('emissiveMat')
 const ambientLightQuery = ecs.with('light', 'ambientLight')
 let intensity = 0
-const tween = new Tween({ intensity: 0 }).to({ intensity: 1 }, 60_000 * 10).yoyo(true).repeat(Number.POSITIVE_INFINITY).start(time.elapsed).onUpdate(value => intensity = value.intensity).easing(Easing.Quadratic.InOut)
+const tween = new Tween({ intensity: 0 }).to({ intensity: 1 }, 6_000 * 10).yoyo(true).repeat(Number.POSITIVE_INFINITY).start(time.elapsed).onUpdate(value => intensity = value.intensity).easing(Easing.Quadratic.InOut)
 
 export const dayNight = () => {
 	tween.update(time.elapsed)
-
+	const overlay = document.querySelector('.overlay')
+	if (overlay instanceof HTMLElement) {
+		overlay.style.setProperty('--opacity', String((1 - intensity) / 2))
+	}
 	for (const entity of nightLightQuery) {
 		entity.nightLight.intensity = intensity
 	}
