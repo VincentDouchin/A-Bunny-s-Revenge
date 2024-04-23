f262d5df224970dba07163fb1974466b2e1b235e
Vincent Douchin <vincent.douchin@gmail.com>
2024-04-23 08:24:38 +0200

    use shader for weapon arc

--- src/shaders/weaponArc.ts
+++ src/shaders/weaponArc.ts
@@ -1,14 +1,11 @@
 import type { Vector3, Vector3Tuple } from 'three'
-import { BufferAttribute, BufferGeometry, DoubleSide, Mesh, MeshBasicMaterial, ShaderMaterial, Uniform } from 'three'
+import { BufferAttribute, BufferGeometry, DoubleSide, Mesh, ShaderMaterial } from 'three'
 import { range } from '@/utils/mapFunctions'
 
-export const WeaponArcMaterial = () => new ShaderMaterial({
+export const weaponArcMaterial = new ShaderMaterial({
 	side: DoubleSide,
 	transparent: true,
 	depthWrite: false,
-	uniforms: {
-		ratio: new Uniform(0),
-	},
 	vertexShader: /* glsl */`
 	varying vec2 vUv;
 	void main() {
@@ -19,15 +16,13 @@ export const WeaponArcMaterial = () => new ShaderMaterial({
 	`,
 	fragmentShader: /* glsl */`
 	uniform sampler2D tDiffuse;
-	uniform float ratio;
 	varying vec2 vUv;
 	void main() {
 		vec2 centered_uv = vUv *2.-1.;
 		
-		float x = 1. - abs(centered_uv.x);
 		float y = 1. - abs(centered_uv.y);
-		float arc = step(0.5,x * y) * (vUv.x < ratio?1.:0.);
-		gl_FragColor = vec4(vec3(arc),arc * 0.2);
+		float arc = step(0.2,vUv.x * y) ;
+		gl_FragColor = vec4(vec3(arc),arc * vUv.x);
 	}
 	`,
 })
@@ -36,7 +31,7 @@ export class WeaponArc extends Mesh {
 	vertices = new Float32Array()
 	lastPoints: [Vector3Tuple, Vector3Tuple] | null = null
 	constructor() {
-		super(new BufferGeometry(), new MeshBasicMaterial({ transparent: true, side: DoubleSide, color: 0xFFFFFF, vertexColors: true, depthWrite: false }))
+		super(new BufferGeometry(), weaponArcMaterial)
 		this.frustumCulled = false
 	}
 
@@ -50,7 +45,7 @@ export class WeaponArc extends Mesh {
 				...top.toArray(),
 				...lt,
 				...lb,
-				...this.vertices.slice(0, 64 * 3),
+				...this.vertices.slice(0, 128 * 3),
 			])
 			this.setVertices()
 		}
@@ -59,11 +54,14 @@ export class WeaponArc extends Mesh {
 
 	setVertices() {
 		const nbVertices = this.vertices.length / (6 * 3)
-		const colors = range(0, nbVertices, (i) => {
-			return [i, i, i + 1, i, i + 1, i + 1].flatMap(o => [1, 1, 1, 1 - o / (nbVertices * 2) - 0.2])
+		const uv = range(0, nbVertices, (i) => {
+			const x1 = 1 - (i * 2) / (nbVertices * 2)
+			const x2 = 1 - (i * 2 + 1) / (nbVertices * 2)
+			return [[x1, 1], [x1, 0], [x2, 0], [x1, 1], [x2, 1], [x2, 0]].flat()
 		}).flat()
+
+		this.geometry.setAttribute('uv', new BufferAttribute(new Float32Array(uv), 2))
 		this.geometry.setAttribute('position', new BufferAttribute(this.vertices, 3))
-		this.geometry.setAttribute('color', new BufferAttribute(new Float32Array(colors), 4))
 	}
 
 	removeVertices() {
