87c90e93fd510cc8c2e26cef9af709c1ddcfaca5
Vincent Douchin <vincent.douchin@gmail.com>
2024-06-22 22:09:13 +0200

    fix errors ui

--- src/ui/Errors.tsx
+++ src/ui/Errors.tsx
@@ -1,9 +1,16 @@
 import { For } from 'solid-js'
+import { css } from 'solid-styled'
 import { errors } from './UI'
 
 export const Errors = () => {
+	css/* css */`
+	.errors{
+		position:fixed;
+		inset:0;
+	}
+	`
 	return (
-		<div>
+		<div class="errors">
 			<For each={errors}>
 				{(error) => {
 					return <div>{error}</div>
