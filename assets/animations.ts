2f6209ca5de6b91a529b71d69881fb6b5eb76151
Vincent Douchin <vincent.douchin@gmail.com>
2025-11-12 08:50:08 +0100

    remove old assets

--- assets/animations.ts
+++ assets/animations.ts
@@ -9,8 +9,6 @@ export interface Animations {
 
 'Armabee_Evolved' : 'Death' | 'Fast_Flying' | 'Flying_Idle' | 'Headbutt' | 'HitReact' | 'No' | 'Punch' | 'Yes'
 
-'Basket' : 'LidClose' | 'LidOpen' | 'WingFlap_Rocking' | 'WingFlapBounce'
-
 'Batty_A' : 'Attack' | 'Damage' | 'Die' | 'Idle' | 'Move'
 
 'Batty_B' : 'Attack' | 'Damage' | 'Die' | 'Idle' | 'Move'
@@ -25,11 +23,7 @@ export interface Animations {
 
 'Big_Boar_C' : 'Attack' | 'Damage' | 'Die' | 'Idle' | 'Move'
 
-'Bunny' : 'ATTACK_NEW_1' | 'ATTACK_NEW_2' | 'CHEER' | 'DEATH' | 'FIGHT_ACTION1' | 'HEAVYATTACK' | 'HURT' | 'IDLE_NEW' | 'NEW_ATTACK_3' | 'RUN_ALT' | 'SLASH' | 'SLASH.001' | 'STAB'
-
 'BunnyClothed' : '1H_Melee_Attack_Chop' | '1H_Melee_Attack_Slice_Diagonal' | '1H_Melee_Attack_Slice_Horizontal' | '1H_Melee_Attack_Stab' | '1H_Ranged_Aiming' | '1H_Ranged_Reload' | '1H_Ranged_Shoot' | '1H_Ranged_Shooting' | '2H_Melee_Attack_Chop' | '2H_Melee_Attack_Slice' | '2H_Melee_Attack_Spin' | '2H_Melee_Attack_Spinning' | '2H_Melee_Attack_Stab' | '2H_Melee_Idle' | '2H_Ranged_Aiming' | '2H_Ranged_Reload' | '2H_Ranged_Shoot' | '2H_Ranged_Shooting' | 'Block' | 'Block_Attack' | 'Block_Hit' | 'Blocking' | 'Cheer' | 'Death_A' | 'Death_A_Pose' | 'Death_B' | 'Death_B_Pose' | 'Dodge_Backward' | 'Dodge_Forward' | 'Dodge_Left' | 'Dodge_Right' | 'Dualwield_Melee_Attack_Chop' | 'Dualwield_Melee_Attack_Slice' | 'Dualwield_Melee_Attack_Stab' | 'Hit_A' | 'Hit_B' | 'Idle' | 'Interact' | 'Jump_Full_Long' | 'Jump_Full_Short' | 'Jump_Idle' | 'Jump_Land' | 'Jump_Start' | 'Lie_Down' | 'Lie_Idle' | 'Lie_Pose' | 'Lie_StandUp' | 'PickUp' | 'Running_A' | 'Running_B' | 'Running_Strafe_Left' | 'Running_Strafe_Right' | 'Sit_Chair_Down' | 'Sit_Chair_Idle' | 'Sit_Chair_Pose' | 'Sit_Chair_StandUp' | 'Sit_Floor_Down' | 'Sit_Floor_Idle' | 'Sit_Floor_Pose' | 'Sit_Floor_StandUp' | 'Spellcast_Long' | 'Spellcast_Raise' | 'Spellcast_Shoot' | 'Spellcasting' | 'T-Pose' | 'Throw' | 'Unarmed_Idle' | 'Unarmed_Melee_Attack_Kick' | 'Unarmed_Melee_Attack_Punch_A' | 'Unarmed_Melee_Attack_Punch_B' | 'Unarmed_Pose' | 'Use_Item' | 'Walking_A' | 'Walking_B' | 'Walking_Backwards' | 'Walking_C'
-
-'BunnyNewRig' : 'CHEER' | 'FIGHT_ACTION1' | 'HEAVYATTACK' | 'HURT' | 'IDLE_NEW' | 'RUN_ALT' | 'SLASH'
 
 'BunnyOvenPacked' : 'Closing' | 'Closing_OvenArmature' | 'Opening' | 'Opening_OvenArmature'
 
